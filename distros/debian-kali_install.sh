687e8cb728e943fe209b682966f4396d285f1d23
Josh Schiavone <jshschiavone@gmail.com>
2023-12-17 02:05:27 -0500

    added installers for kali/debian

---
+++ distros/debian-kali_install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python3-tk
+sudo apt-get install python3-pil python3-pil.imagetk
+sudo apt-get install build-essential libgl1-mesa-dev
+pip3 install pyqt5 pyqtwebengine
+pip3 install pywebview
+pip3 install pywebview[qt]
+pip3 install qtpy pyside2
+sudo apt install python3-pyqt5 python3-pyqt5.qtwebengine python3-pyqt5.qtwebchannel libqt5webkit5-dev
+
